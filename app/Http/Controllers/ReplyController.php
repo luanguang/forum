2f8d720172a965d9c4d78a9e8e571bba9247a618
luanguang <523086505@qq.com>
2018-10-17 17:20:42 +0800

    import vue delete

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -40,6 +40,10 @@ class ReplyController extends Controller
 
         $reply->delete();
 
+        if (request()->expectsJson()) {
+            return response((['status' => 'Reply deleted']));
+        }
+
         return back();
     }
 }
